f4b658afc015344b5b4119c43ca743c9b01e12c7
Millie <Millie@MacBook-Pro-Kamilla.local>
2022-03-20 21:34:02 +0300

    Не терять равновесие

---
+++ libft/ft_calloc.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbenjami <nbenjami@student.21-school.ru>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/21 18:30:18 by nbenjami          #+#    #+#             */
+/*   Updated: 2022/03/20 16:51:42 by nbenjami         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*p;
+
+	p = malloc (count * size);
+	if (p == 0)
+	{
+		free (p);
+		return (NULL);
+	}	
+	ft_bzero (p, count * size);
+	return (p);
+}
